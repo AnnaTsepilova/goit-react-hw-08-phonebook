751d61ec4f8202ccb6f921c335896fffcdbe733d
Anna Tsepilova <virgo.tsav@gmail.com>
2023-01-21 03:11:30 +0200

    add SignIn & SignIUp pages

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -13,7 +13,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+//import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { GithubLogo, SignUpLink } from 'components/SignIn/SignIn.styled';
