9ad33555ee5dad855b95da95d389872a38c64fe7
Anna Tsepilova <virgo.tsav@gmail.com>
2023-01-24 14:44:52 +0200

    Fixed basename

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-05-movies">
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             {/* <BrowserRouter> */}
             <App />
           </BrowserRouter>
